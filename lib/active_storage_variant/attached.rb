c48e502fa8d238d4682a58d759993af953e45195
Jan Klimke <jklimke@users.noreply.github.com>
2025-06-23 14:51:21 +0200

    Update attached.rb
    Fixed passing option arguments to has_one_attached and has_many_attached.

--- lib/active_storage_variant/attached.rb
+++ lib/active_storage_variant/attached.rb
@@ -3,13 +3,13 @@ module ActiveStorageVariant
     extend ActiveSupport::Concern
 
     class_methods do
-      def has_one_attached(name, *arg)
+      def has_one_attached(name, **arg)
         super
         reflection = self.reflect_on_attachment(name)
         yield reflection if block_given?
       end
 
-      def has_many_attached(name, *arg)
+      def has_many_attached(name, **arg)
         super
         reflection = self.reflect_on_attachment(name)
         yield reflection if block_given?
